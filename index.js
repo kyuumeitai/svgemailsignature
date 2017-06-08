2b9d12ad8e93da74f7d717f9044172ae3e263089
Leonardo Gatica <lgatica@protonmail.com>
2017-06-08 09:09:28 +0000

    chore(streams): se migro codigo a streams
    Se agrego eslint con standard y prettier
    Se agrego scripts start, lint y format
    Se corrigio render de logo
    Se eliminaron dependecias sin uso
    Se agrego package-lock.json

--- index.js
+++ index.js
@@ -1,103 +1,90 @@
-"use strict";
+'use strict'
 
-const express = require('express');
-const fs = require('pn/fs');
-const svg_to_png = require('svg-to-png');
-const s = require("underscore.string");
-const expressValidator = require('express-validator');
-const bodyParser = require('body-parser');
-const app = express();
-const util = require('util');
-const mkdirp = require('mkdirp');
-const gm = require('gm');
-const path = require('path');
-const Promise = require('bluebird');
-const {execFile} = require('child_process');
-const gifsicle = require('gifsicle');
+const fs = require('fs')
+const path = require('path')
+const { inspect } = require('util')
+const express = require('express')
+const dasherize = require('underscore.string/dasherize')
+const trim = require('underscore.string/trim')
+const cleanDiacritics = require('underscore.string/cleanDiacritics')
+const expressValidator = require('express-validator')
+const mkdirp = require('mkdirp-then')
+const gm = require('gm')
+const Promise = require('bluebird')
+const replaceStream = require('replacestream')
 
-Promise.promisifyAll(gm.prototype);
+const imageMagick = gm.subClass({ imageMagick: true })
+const app = express()
+Promise.promisifyAll(gm.prototype)
 
-let info = {};
-let config = {
-  // url : 'http://people.mad.cl',
-  url : 'http://localhost:8081',
-  static : 'public',
-  path : 'signature'
-};
+const info = {}
+const config = {
+  url: 'http://localhost:8081',
+  static: 'public',
+  path: 'signature'
+}
 
-app.use(expressValidator());
-app.use(express.static(config.static));
-app.set('views', './views');
-app.set('view engine', 'pug');
+app.use(expressValidator())
+app.use(express.static(config.static))
+app.set('views', './views')
+app.set('view engine', 'pug')
 
-app.get('/', function(req, res){
-  //obligatorios
-  
-  req.checkQuery('nombre', 'No viene nombre').notEmpty();
-  req.checkQuery('cargo', 'No viene cargo').notEmpty();
-  
-  info.direccion = req.query.direccion || 'Av. Nueva Providencia 1860 oficina 173';
-  info.telefono = req.query.telefono || '+569 28984940';
-  info.celular = req.query.celular || '';
-  info.sitio = req.query.sitio || 'www.mad.cl';
-  
-
-  var errors = req.validationErrors();
-  if(errors){
-    res.status(400).send('errores:' + util.inspect(errors));
-    return;
+app.get('/', (req, res) => {
+  // obligatorios
+  req.checkQuery('nombre', 'No viene nombre').notEmpty()
+  req.checkQuery('cargo', 'No viene cargo').notEmpty()
+  info.direccion =
+    req.query.direccion || 'Av. Nueva Providencia 1860 oficina 173'
+  info.telefono = req.query.telefono || '+569 28984940'
+  info.celular = req.query.celular || ''
+  info.sitio = req.query.sitio || 'www.mad.cl'
+  const errors = req.validationErrors()
+  if (errors) {
+    return res.status(400).send(`errores: ${inspect(errors)}`)
   }
-
-  info.nombre = req.query.nombre;
-  info.cargo = req.query.cargo;
-  info.slug = s.dasherize(s.trim(s.cleanDiacritics(info.nombre).toLowerCase()));
-  info.permalink = config.url +'/'+ info.slug +'/'+ config.path;
-
-  console.log(info);
-  fs.readFile("firma-base.svg", 'utf8')
-    .then(source =>{
-      var result = source.replace(/::NOMBRE::/g, info.nombre.toUpperCase());
-      result = result.replace(/::CARGO::/g, info.cargo.toUpperCase());
-      result = result.replace(/::DIRECCION::/g, info.direccion);
-      result = result.replace(/::TELEFONO::/g, info.telefono);
-      result = result.replace(/::CELULAR::/g, info.celular);      //IF EMPTY
-      result = result.replace(/::SITIO::/g, info.sitio);
-      console.log('result');
-      return result;
-    }
-  )
-  .then(result => {
-    mkdirp(config.static+'/'+info.slug+'/'+config.path, function (err) {
-      if (err) throw err;
-      console.log('mkdirp');
-      const userpath = path.resolve(path.join(__dirname, config.static+'/'+info.slug+'/'+config.path));
-      const filename = userpath + '/' + info.slug;
-
-      fs.writeFile(filename+'-alt.svg', result, 'utf8', (err) =>{
-        if(err) throw err;
-        console.log('svg!');
-        svg_to_png.convert(filename + '-alt.svg', userpath)
-          .then(function(){
-            console.log('trim');
-            gm(filename + '-alt.png')
-            .trim()
-            .write(filename + '-alt.png', function(err){
-              console.log('trimmed');
-              if(err) throw err;
-              gm(filename+'-alt.png').size(function(err, size){
-                if(err) throw err;
-                res.render('index', { nombre: info.nombre, permalink: info.permalink, slug: info.slug , url: 'http://'+info.sitio, size: size, localurl: config.url })
-              })
-            })
-          })
-          .catch(e => console.error(e))
+  info.nombre = req.query.nombre
+  info.cargo = req.query.cargo
+  info.slug = dasherize(trim(cleanDiacritics(info.nombre).toLowerCase()))
+  info.permalink = `${config.url}/${info.slug}/${config.path}`
+  const userpath = path.join(__dirname, config.static, info.slug, config.path)
+  const filename = path.join(userpath, info.slug)
+  mkdirp(userpath)
+    .then(() => {
+      const svgStream = fs
+        .createReadStream(path.join(__dirname, 'firma-base.svg'))
+        .pipe(replaceStream(/::NOMBRE::/g, info.nombre.toUpperCase()))
+        .pipe(replaceStream(/::CARGO::/g, info.cargo.toUpperCase()))
+        .pipe(replaceStream(/::DIRECCION::/g, info.direccion))
+        .pipe(replaceStream(/::TELEFONO::/g, info.telefono))
+        .pipe(replaceStream(/::CELULAR::/g, info.celular))
+        .pipe(replaceStream(/::SITIO::/g, info.sitio))
+      return imageMagick(svgStream).trim().streamAsync('png')
+    })
+    .then((stdout, stderr) => {
+      return new Promise((resolve, reject) => {
+        const pngStream = fs.createWriteStream(`${filename}-alt.png`)
+        stdout.pipe(pngStream)
+        pngStream.on('finish', () => {
+          const rs = fs.createReadStream(`${filename}-alt.png`)
+          return imageMagick(rs).sizeAsync().then(resolve).catch(reject)
+        })
       })
     })
-  })
-});
+    .then(size => {
+      res.render('index', {
+        nombre: info.nombre,
+        permalink: info.permalink,
+        slug: info.slug,
+        url: `http://${info.sitio}`,
+        size: size,
+        localurl: config.url
+      })
+    })
+    .catch(err => console.error(err))
+})
 
-var server = app.listen(8081, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Signatures in http://%s:%s", host, port)
+const server = app.listen(8081, () => {
+  const host = server.address().address
+  const port = server.address().port
+  console.log(`Signatures in http://${host}:${port}`)
 })
